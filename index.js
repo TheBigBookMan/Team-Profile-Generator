94532685be7c0d3a004f7abc8253ee859682f37b
Ben Smerd <bsmerd@adam.com.au>
2022-08-10 21:05:29 +0930

    added in the manager prompt to index.js

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 // Install packages
 const inquirer = require('inquirer');
-console.log(inquirer)
+const fs = require('fs');
 
 // NEED TO HAVE TEST JS FILES AS WELL
 
 
 
 // APP STARTS WITH PROMPTING ABOUT THE MANAGERS INFO
-
+// CAN TRY DO THE VALIDATION BUT MAYBE LEAVE THAT FOR LATER IF HAVE TIME
+// Function that prompts the user about manager information
+const promptManager = () => {
+    return inquirer.prompt([{
+        type: "input",
+        message: "What is the team manager's name?",
+        name: "managerName"
+    }, {
+        type: "number",
+        message: "What is the team manager's id?",
+        name: "managerId"
+    }, {
+        type: "input",
+        message: "What is the team manager's email?",
+        name: "managerEmail"
+    }, {
+        type: "number",
+        message: "What is the team manager's office number?",
+        name: "managerOfficeNum"
+    }])
+    .then((data) => {
+        
+    })
+}
 
 
 // AFTER MANAGER INFO THEN ASKED IF THEY WANT TO ADD IN ENGINEER, INTERN OR NO ONE
@@ -20,4 +43,11 @@ console.log(inquirer)
 
 
 // AFTER SELECTING ADD NO MORE MEMBERS IT THEN RENDERS THE HTML AND CSS FILES OF THE EMPLOYEES
+// Function that is called on initialization and starts the application
+const init = () =>{
+    console.log("Welcome to the team profile generator! \nUse 'npm run reset' to reset the dist/ folder.\n\nPlease build your team here!");
+    promptManager();
+}
 
+
+init();
